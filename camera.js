1a6c48057f8367d6a12c442d19298e05b4d48f3a
Yuto Mori <b1016206@fun.ac.jp>
2019-03-02 13:51:17 +0900

    [change and clean] 公式のデモに近づけた

--- camera.js
+++ camera.js
@@ -1,120 +1,143 @@
 const imageScaleFactor = 0.2;
 const outputStride = 16;
 const flipHorizontal = false;
-const stats = new Stats();
-const VideoWidth = 800;
-const VideoHeight = 600;
+const videoWidth = 600;
+const videoHeight = 600;
+const color = "aqua";
 
 let imageScale = 1;
 let imageFace = new Image();
-imageFace.src = "image.png"
+imageFace.src = "image.png";
 
 // カメラのセットアップ
 async function setupCamera() {
-    const video = document.getElementById('video');
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        const stream = await navigator.mediaDevices.getUserMedia({
-            'audio': false,
-            'video': true});
-        video.srcObject = stream;
+  const video = document.getElementById("video");
+  video.width = videoWidth;
+  video.height = videoHeight;
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: "user",
+        width: videoWidth,
+        height: videoHeight
+      }
+    });
+    video.srcObject = stream;
 
-        return new Promise(resolve => {
-            video.onloadedmetadata = () => {
-                resolve(video);
-            };
-        });
-    } else {
-        const errorMessage = "This browser does not support video capture, or this device does not have a camera";
-        alert(errorMessage);
-        return Promise.reject(errorMessage);
-    }
+    return new Promise(resolve => {
+      video.onloadedmetadata = () => {
+        resolve(video);
+      };
+    });
+  } else {
+    const errorMessage =
+      "This browser does not support video capture, or this device does not have a camera";
+    alert(errorMessage);
+    return Promise.reject(errorMessage);
+  }
 }
 
 // ビデオの起動
 async function loadVideo() {
-    const video = await setupCamera();
-    video.play();
-    return video;
+  const video = await setupCamera();
+  video.play();
+  return video;
 }
 
 // 姿勢予測
 function detectPoseInRealTime(video, net) {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    const flipHorizontal = true; 
-    // since images are being fed from a webcam
+  const canvas = document.getElementById("output");
+  const ctx = canvas.getContext("2d");
+  const flipHorizontal = true;
+  // since images are being fed from a webcam
 
-    async function poseDetectionFrame() {
-        stats.begin();
-        let poses = [];
-        const pose = await net.estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride);
-        // 複数人での認識はここを修正する
-        poses.push(pose);
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
 
-        ctx.clearRect(0, 0, VideoWidth,VideoHeight);
+  async function poseDetectionFrame() {
+    let poses = [];
+    let minPoseConfidence = 0.1; // if single pose detection
+    let minPartConfidence = 0.5;
 
-        ctx.save();
-        ctx.scale(-1, 1);
-        ctx.translate(-VideoWidth, 0);
-        ctx.drawImage(video, 0, 0, VideoWidth, VideoHeight);
-        ctx.restore();
+    const pose = await net.estimateSinglePose(
+      video,
+      imageScaleFactor,
+      flipHorizontal,
+      outputStride
+    );
+    // 複数人での認識はここを修正する
+    poses.push(pose);
 
-        poses.forEach(({score, keypoints }) => {
-            drawFace(keypoints[0], keypoints[1], ctx);
-            drawWristPoint(keypoints[0],ctx);　// nose
-            drawWristPoint(keypoints[1],ctx);  // left Eye
-            drawWristPoint(keypoints[2],ctx);  // right Eye
-            drawWristPoint(keypoints[3],ctx);  // left Ear
-            drawWristPoint(keypoints[4],ctx);  // right Ear
-            drawWristPoint(keypoints[5],ctx);  // left Shoulder
-            drawWristPoint(keypoints[6],ctx);  // right Shoulder
-            drawWristPoint(keypoints[7],ctx);  // left Elbow
-            drawWristPoint(keypoints[8],ctx);  // right Elbow
-            drawWristPoint(keypoints[9],ctx);  // left Wrist
-            drawWristPoint(keypoints[10],ctx);  // right Wrist
-            drawWristPoint(keypoints[11],ctx);  // left Hip
-            drawWristPoint(keypoints[12],ctx);  // right Hip
-            drawWristPoint(keypoints[13],ctx);  // left Knee
-            drawWristPoint(keypoints[14],ctx);  // right Knee
-            drawWristPoint(keypoints[15],ctx);  // left Ankle
-            drawWristPoint(keypoints[16],ctx);  // right Ankle
-        });
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
 
-        stats.end();
+    ctx.save();
+    ctx.scale(-1, 1);
+    ctx.translate(-videoWidth, 0);
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+    ctx.restore();
 
-        requestAnimationFrame(poseDetectionFrame);
-    }
-    poseDetectionFrame();
+    poses.forEach(({ score, keypoints }) => {
+      drawFace(keypoints[0], keypoints[1], ctx);
+      if (score >= minPoseConfidence) {
+        drawKeypoints(keypoints, minPartConfidence, ctx);
+      }
+    });
+
+    requestAnimationFrame(poseDetectionFrame);
+  }
+  poseDetectionFrame();
 }
 
 // Posenetのモデルを読み込んでカメラを開始する
 async function bindPage() {
-    const net = await posenet.load(); // posenetの呼び出し
-    let video;
-    try {
-        video = await loadVideo(); // video属性をロード
-    } catch(e) {
-        console.error(e);
-        return;
-    }
-    detectPoseInRealTime(video, net);
+  const net = await posenet.load(); // posenetの呼び出し
+
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  let video;
+  try {
+    video = await loadVideo(); // video属性をロード
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+  detectPoseInRealTime(video, net);
 }
 
-// 与えられたKeypointをcanvasに描画する
-function drawWristPoint(wrist,ctx){
-    ctx.beginPath();
-    ctx.arc(wrist.position.x , wrist.position.y, 3, 0, 2 * Math.PI);
-    ctx.fillStyle = "pink";
-    ctx.fill();
+function drawPoint(ctx, y, x, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+  for (let i = 0; i < keypoints.length; i++) {
+    const keypoint = keypoints[i];
+
+    if (keypoint.score < minConfidence) {
+      continue;
+    }
+
+    const { y, x } = keypoint.position;
+    drawPoint(ctx, y * scale, x * scale, 3, color);
+  }
 }
 
 // 顔に写真を貼り付ける
-function drawFace(nose, leye, ctx){
-    imageScale = (leye.position.x - nose.position.x - 50) / 50;
-    if (imageScale < 0.7) imageScale = 0.7;
-    let nw = imageFace.width * imageScale;
-    let nh = imageFace.height * imageScale;
-    ctx.drawImage(imageFace,nose.position.x - nh / 2 , nose.position.y - nh / 1.5, nw, nh);
+function drawFace(nose, leye, ctx) {
+  imageScale = (leye.position.x - nose.position.x - 50) / 50;
+  if (imageScale < 0.7) imageScale = 0.7;
+  let nw = imageFace.width * imageScale;
+  let nh = imageFace.height * imageScale;
+  ctx.drawImage(
+    imageFace,
+    nose.position.x - nh / 2,
+    nose.position.y - nh / 1.5,
+    nw,
+    nh
+  );
 }
 
 bindPage();
